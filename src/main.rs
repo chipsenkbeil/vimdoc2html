7b63d18af6e8d2ff781b51a4379d5120a35adf8a
Chip Senkbeil <chip@senkbeil.org>
2022-12-29 14:57:19 -0600

    Add cli implementation minus core html conversion

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ struct Args {
     #[arg(short, long)]
     recursive: bool,
 
+    /// If specified, will write out a debug string instead of HTML.
+    #[arg(long)]
+    debug_output: bool,
+
     /// Paths to convert from vimdoc into html. If no paths are provided, will read vimdoc from
     /// stdin until EOF detected and then print out the html.
     paths: Vec<PathBuf>,
@@ -25,6 +29,7 @@ fn main() {
     let Args {
         extensions,
         recursive,
+        debug_output,
         paths,
     } = <Args as clap::Parser>::parse();
     let should_read_stdin = paths.is_empty();
@@ -35,7 +40,12 @@ fn main() {
     // then print out the results
     if should_read_stdin {
         let tree = parse_into_tree(std::io::stdin(), &mut parser).unwrap();
-        println!("{tree:#?}");
+        let out = if debug_output {
+            tree_into_debug_string(tree, /* pretty */ true)
+        } else {
+            tree_into_html_string(tree)
+        };
+        println!("{out}");
         return;
     }
 
@@ -48,7 +58,12 @@ fn main() {
             let outfile = path.with_extension("html");
             let tree = parse_into_tree(File::open(path).expect("Failed to open file"), &mut parser)
                 .unwrap();
-            std::fs::write(outfile, format!("{tree:#?}")).expect("Failed to write output");
+            let out = if debug_output {
+                tree_into_debug_string(tree, /* pretty */ true)
+            } else {
+                tree_into_html_string(tree)
+            };
+            std::fs::write(outfile, out).expect("Failed to write output");
         } else if path.is_dir() {
             for entry in std::fs::read_dir(path).expect("Failed to read directory") {
                 let entry = entry.expect("Failed to read directory entry");
@@ -86,4 +101,71 @@ fn parse_into_tree<R: io::Read>(
     parser
         .parse(buf, None)
         .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Failed to parse vimdoc"))
+}
+
+/// Converts [`tree_sitter::Tree`] into a debug [`String`]. If `pretty` is true, will perform line
+/// breaks and indentation for the output.
+fn tree_into_debug_string(tree: tree_sitter::Tree, pretty: bool) -> String {
+    // Creates an sexp string that is all on one line
+    let raw_out = tree.root_node().to_sexp();
+
+    if pretty {
+        pretty_print(tree.root_node(), /* show_anonymous */ true)
+    } else {
+        raw_out
+    }
+}
+
+/// Converts [`tree_sitter::Tree`] into an HTML [`String`].
+fn tree_into_html_string(_tree: tree_sitter::Tree) -> String {
+    todo!();
+}
+
+// From https://github.com/jedthehumanoid/hecto
+fn pretty_print(node: tree_sitter::Node, show_anonymous: bool) -> String {
+    let mut cursor = node.walk();
+    let mut indent = String::new();
+    let mut ret = String::new();
+    loop {
+        if cursor.node().is_named() || show_anonymous {
+            ret += &format!("{}{}\n", indent, cursor_pretty(&cursor));
+        }
+
+        if cursor.goto_first_child() {
+            indent += "  ";
+            continue;
+        }
+        if cursor.goto_next_sibling() {
+            continue;
+        }
+
+        // Retrace upwards until additional siblings are avaliable
+        loop {
+            if !cursor.goto_parent() {
+                return ret;
+            }
+            indent = indent[0..indent.len() - 2].to_string();
+
+            if cursor.goto_next_sibling() {
+                break;
+            }
+        }
+    }
+}
+
+// From https://github.com/jedthehumanoid/hecto
+fn cursor_pretty(cursor: &tree_sitter::TreeCursor) -> String {
+    let field_name = match cursor.field_name() {
+        Some(name) => String::from(name) + ": ",
+        None => String::from(""),
+    };
+    format!(
+        "Name: {:?}, Kind: {:?} [Row:{}, Col:{}] - [Row:{}, Col:{}]",
+        field_name,
+        cursor.node().kind(),
+        cursor.node().start_position().row,
+        cursor.node().start_position().column,
+        cursor.node().end_position().row,
+        cursor.node().end_position().column,
+    )
 }
